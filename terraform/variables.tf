a8d95b81b8fa83773f59e98ac4a59cd86842a05f
Dakuchi <datngthanh98@gmail.com>
2024-10-07 09:42:20 +0000

    update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,7 +8,7 @@ variable "vpc_cidr_block" {
   default = "10.0.0.0/16"
 }
 variable "subnet_cidr_block" {
-  default = "10.0.10.0/16"
+  default = "10.0.10.0/24"
 }
 variable "env_prefix" {
   default = "dev"
